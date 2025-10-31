44102d90062fc34eeb6304f7cb9ab05b7dce27d6
Sawan Chouhan <sawanchouhan751@gmail.com>
2025-10-31 12:41:09 +0530

    `Added painter's partition problem solution in C++`

---
+++ Painter's partition.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <vector>
+#include <iostream>
+using namespace std;
+bool isPossible(int wall[],int painters,int n, int maxload){
+    int reqpainters= 1;
+    int currentsum=0;
+    for(int i=0;i<n;i++){
+        if(currentsum+wall[i]<=maxload){
+            currentsum+=wall[i];
+        }
+        else{
+            reqpainters++;
+            currentsum=wall[i];
+            if(reqpainters>painters || wall[i]>maxload){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+int main() {
+    int  wall[] = {40,30,20,10}; 
+    int n = sizeof(wall)/sizeof(wall[0]);
+    int st= 0;
+    int end = 0;
+    int ans = -1;
+    int painters = 2;
+    for (int i = 0; i < n; i++) {
+        end += wall[i];
+    }
+    while(st <= end){
+        int mid = (st + end) / 2;
+        if(isPossible(wall, painters, n, mid)){
+            ans = mid;
+            end = mid - 1;
+        }
+        else{
+            st = mid + 1;
+        }
+    }
+    cout << "Minimum time required to paint all walls: " << ans << endl;
+    printf("%d\n",ans);
+    return 0;
+}
