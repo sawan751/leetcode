8e3b22c0f439cd66b9f2ba5e6e7041bc65cde4ac
Sawan Chouhan <sawanchouhan751@gmail.com>
2025-11-09 01:33:23 +0530

    Time: 0 ms (100%), Space: 12.5 MB (44.98%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        s = s.split()
+        return len(s[-1])
+        
