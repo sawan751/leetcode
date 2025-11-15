269d06575ced19cb0c232368e16632539fbbc922
Sawan Chouhan <sawanchouhan751@gmail.com>
2025-11-15 21:53:40 +0530

    Time: 0 ms (100%), Space: 16.8 MB (80.53%) - LeetHub

---
+++ 0001-concatenation-of-array/0001-concatenation-of-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(2*n);
+        int i = 0;
+        while(i<n){
+            ans[i] = nums[i];
+            ans[i+n] = nums[i];
+            i++;
+
+        }
+        return ans;
+    }
+};
