b362f12679380b958f80c711b3292f3d7b24a81d
Sawan Chouhan <sawanchouhan751@gmail.com>
2025-11-19 13:48:20 +0530

    Time: 3 ms (46.16%), Space: 8.4 MB (92.13%) - LeetHub

---
+++ 0003-palindrome-number/0003-palindrome-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0) return false; // negative numbers are not palindrome
+        
+        int original = x;
+        long rev = 0;
+        
+        while(x > 0){
+            rev = rev * 10 + (x % 10);
+            x /= 10;
+        }
+        
+        return rev == original;
+    }
+};
