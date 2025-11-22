ae82f33c54fe6dc7e7bbf9d0ae2dc9a94b1c157e
Sawan Chouhan <sawanchouhan751@gmail.com>
2025-11-22 09:37:14 +0530

    Time: 19 ms (24.86%), Space: 31.7 MB (37.6%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
@@ -4,19 +4,20 @@ public:
         unordered_map<int,int> freq;
         int n = grid.size();
         int total = n*n;
+        int actsum = 0;
+//formua for finding missing value is expectedsum + duplicate -acutalsum
         int missing = 0, duplicate = 0;
         for(int r = 0;r<n;r++){
             for(int c = 0;c<n;c++){
                 int val = grid[r][c];
+                actsum += val;
                 freq[val]++;
                 if(freq[val]>1) duplicate = val;
             }
-        } 
-        for(int i = 1;i<=total;i++){
-            if(freq.find(i) == freq.end()){
-                missing = i;
-            }
         }
+        int expsum = (n*n)*(n*n+1)/2;
+        missing = expsum +duplicate -actsum;
+
         return {duplicate,missing};
     }
 };
