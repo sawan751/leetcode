d31e2340d8770c4eeb467f52574ef0e12beba99a
Sawan Chouhan <sawanchouhan751@gmail.com>
2025-12-27 20:34:07 +0530

    811 ms (13.88%) 13.6 MB (9.77%) 12-27-2025 Easy

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.py
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.py
@@ -5,17 +5,17 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        res = []
-        i = len(num) - 1
-        carry = k
+        ans = []
+        sum = ''
+        for i in num:
+            sum += str(i)
+        sum = int(sum) +k
 
-        # Add digits from the end of num with the value k (in carry)
-        while i >= 0 or carry:
-            if i >= 0:
-                carry += num[i]
-                i -= 1
-            res.append(carry % 10)
-            carry //= 10
+        while(sum != 0):
+            d = sum%10
+            ans.append(d)
+            sum = sum//10
+        
+        return ans[::-1]
 
-        # res currently holds digits in reverse order
-        return res[::-1]
+        
