d4f81f6a4d4f7aaad269bee8587a1c25cb331a06
Sawan Chouhan <sawanchouhan751@gmail.com>
2026-01-15 16:16:42 +0530

    0 ms (100%) 12.5 MB (51.96%) 01-15-2026 Easy

--- 0415-add-strings/0415-add-strings.py
+++ 0415-add-strings/0415-add-strings.py
@@ -5,28 +5,4 @@ class Solution(object):
         :type num2: str
         :rtype: str
         """
-        if num1 == "0" and num2 == "0":
-            return "0"
-        if num2 == "0" or num1 == "0":
-            if num2 == "0":
-                return num1
-            else:
-                return num2
-        i = len(num1)
-        j = len(num2)
-        carry = 0
-        ans = []
-        while(i> 0 or j>0 or carry >0):
-            s = carry
-            
-            if i>0:
-                s += int(num1[i-1])
-                i-=1
-            if j>0:
-                s +=int(num2[j-1])
-                j-=1
-            
-            carry = s //10
-            ans.append(s%10)
-        ans.reverse()
-        return ''.join(map(str,ans))
+        return str(int(num1)+int(num2))
