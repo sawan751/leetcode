f5c39b5d91e80dd0671c45b83fdac52e1c3c7c62
Sawan Chouhan <sawanchouhan751@gmail.com>
2026-01-26 11:52:10 +0530

    29 ms (38.57%) 21.4 MB (32.69%) 01-26-2026 Easy

---
+++ 0561-array-partition/0561-array-partition.py
@@ -0,0 +1,9 @@
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        nums = sorted(nums)
+        s = 0
+        i = 1
+        while(i<len(nums)):
+            s += min(nums[i],nums[i-1])
+            i+=2
+        return s
