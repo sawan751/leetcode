c6d0de25606406f97aacc24443327219004e55d2
Sawan Chouhan <sawanchouhan751@gmail.com>
2026-02-17 10:10:49 +0530

    0 ms (100%) 11.8 MB (82.49%) 02-17-2026 Easy

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string ans = "";
+        for(int i = 0;i<strs[0].size();i++){
+            for(int j = 1;j<strs.size();j++){
+                if(strs[j][i] != strs[j-1][i]){
+                    return ans;
+                }
+            }
+            ans += strs[0][i];
+
+        }
+        return ans;
+    }
+};
