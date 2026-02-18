4df8f68c0978eadbfae5689bbd9be468323605dd
Sawan Chouhan <sawanchouhan751@gmail.com>
2026-02-18 22:11:25 +0530

    0 ms (100%) 7.8 MB (80.69%) 02-18-2026 Medium

---
+++ 0002-smallest-integer-divisible-by-k/0002-smallest-integer-divisible-by-k.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int smallestRepunitDivByK(int k) {
+        long long val = 0;
+        for(int i =1;i<=k;i++){
+            val = (val*10+1)%k;
+            if(val == 0) return i;
+        }
+        return -1;
+    }
+};
