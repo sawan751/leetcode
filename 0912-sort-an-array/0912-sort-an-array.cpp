7cf64aba9c947ae807b055cfe874d4ae59ddf576
Sawan Chouhan <sawanchouhan751@gmail.com>
2026-02-24 22:19:13 +0530

    661 ms (20.58%) 295.7 MB (17.2%) 02-24-2026 Medium

---
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    void merge(vector<int> &nums, int st, int mid, int end) {
+        int i = st, j = mid + 1;
+        vector<int> ans;
+        while (i <= mid && j <= end) {
+            if (nums[i] <= nums[j]) {
+                ans.push_back(nums[i]);
+                i++;
+            } else {
+                ans.push_back(nums[j]);
+                j++;
+            }
+        }
+        while(i<=mid){
+            ans.push_back(nums[i]);
+            i++;
+        }
+        while(j<=end){
+            ans.push_back(nums[j]);
+            j++;
+        }
+        for(int idx = 0;idx<ans.size();idx++){
+            nums[idx+st] = ans[idx];
+        }
+    }
+    void splitmid(vector<int> &nums, int st, int end) {
+        if (st < end) {
+            int mid = st + (end - st) / 2;
+
+            splitmid(nums, st, mid);
+            splitmid(nums, mid + 1, end);
+            merge(nums,st,mid,end);
+        }
+    }
+    vector<int> sortArray(vector<int>& nums) {
+
+        splitmid(nums, 0, nums.size() - 1);
+        return nums;
+    }
+};
