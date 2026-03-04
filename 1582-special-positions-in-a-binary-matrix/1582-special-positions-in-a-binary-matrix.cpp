dc6ec9d3c5e7488e8d5646395a81be258383d21e
Sawan Chouhan <sawanchouhan751@gmail.com>
2026-03-04 08:52:47 +0530

    11 ms (5.37%) 17 MB (57.25%) 03-04-2026 Easy

---
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int numSpecial(vector<vector<int>>& mat) {
+        int row = mat.size();
+        int col = mat[0].size();
+        
+        vector<int> r(row,0);
+        vector<int> c(col,0);
+
+        for(int i = 0;i<row;i++){
+            for(int j = 0; j<col;j++){
+                if(mat[i][j] == 1){
+                r[i]++;
+                c[j]++;}
+            }
+        }
+        for(int i = 0;i<row;i++){
+            cout<<"row "<<r[i];
+        }
+        cout<<endl;
+        for(int i = 0;i<col;i++){
+            cout<<"col "<<c[i];
+        }
+        int count = 0;
+        for(int i = 0;i<row;i++){
+            for(int j = 0; j<col;j++){
+                if(mat[i][j]==1 && r[i]==1 && c[j]==1){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
