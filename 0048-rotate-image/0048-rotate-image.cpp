1b2f9cc4a61d72f785500efc0eac5454de11b119
Sawan Chouhan <sawanchouhan751@gmail.com>
2026-05-04 21:50:47 +0530

    0 ms (100%) 10.2 MB (35.34%) 05-04-2026 Medium

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int n2 = (n+1)/2;
+        
+        for(int i = 0;i<n;i++){
+            for(int j = i+1;j<n;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        for(int i= 0;i<n;i++){
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
+    }
+};
