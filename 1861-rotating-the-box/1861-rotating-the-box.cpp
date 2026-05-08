1aae462f7dd6e477211a3f83e5d5a0bd3e4c8691
Sawan Chouhan <sawanchouhan751@gmail.com>
2026-05-08 18:49:53 +0530

    12 ms (53.95%) 56.6 MB (98.55%) 05-08-2026 Medium

--- 1861-rotating-the-box/1861-rotating-the-box.cpp
+++ 1861-rotating-the-box/1861-rotating-the-box.cpp
@@ -4,26 +4,23 @@ public:
         int m = box.size();
         int n = box[0].size();
 
-        for(int i = m-1;i>=0;i--){
-            int empty = n-1;
-            for(int j = n-1;j>=0;j--){
-                if(box[i][j] =='*'){
-                    empty = j-1;
-                }
-                else if(box[i][j] == '#'){
-                    swap(box[i][j],box[i][empty]);
+        for (int i = m - 1; i >= 0; i--) {
+            int empty = n - 1;
+            for (int j = n - 1; j >= 0; j--) {
+                if (box[i][j] == '*') {
+                    empty = j - 1;
+                } else if (box[i][j] == '#') {
+                    swap(box[i][j], box[i][empty]);
                     empty--;
                 }
             }
         }
 
-        //now time to rotate the matrix
-        vector<vector<char>> ans(n,vector<char>(m));
-        for(int i = 0;i<m;i++){
-            for(int j = 0;j<n;j++){
-                ans[j][m-1-i] = box[i][j];
+        vector<vector<char>> ans(n, vector<char>(m));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                ans[j][m - 1 - i] = box[i][j];
             }
-            
         }
         return ans;
     }
