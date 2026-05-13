95c27d62639db4e48d5430e0646cd263a027e34c
Sawan Chouhan <sawanchouhan751@gmail.com>
2026-05-13 23:56:00 +0530

    2 ms (81.89%) 14.1 MB (43.13%) 05-13-2026 Medium

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+
+        if(numRows == 1) return s;
+
+        vector<string> rows(numRows);
+
+        int currRows = 0;
+        int dir = 1;
+
+        for(char c : s) {
+
+            rows[currRows] += c;
+
+            if(currRows == 0)
+                dir = 1;
+
+            else if(currRows == numRows - 1)
+                dir = -1;
+
+            currRows += dir;
+        }
+
+        string ans;
+
+        for(string r : rows) {
+            ans += r;
+        }
+
+        return ans;
+    }
+};
