072b7a7950ef8634bea25a96416365e50e92ecd4
Sawan Chouhan <sawanchouhan751@gmail.com>
2026-05-30 22:50:02 +0530

    0 ms (100%) 12.2 MB (90.67%) 05-30-2026 Easy

---
+++ 2490-circular-sentence/2490-circular-sentence.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isCircularSentence(self, sentence):
+        words = sentence.split()
+
+        for i in range(len(words) - 1):
+            if words[i][-1] != words[i + 1][0]:
+                return False
+
+        return words[-1][-1] == words[0][0]
